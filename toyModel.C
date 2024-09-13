97c75a22df0da15a29927bdc46c0db513d4c9479
Ayush Chakraborty <chak.polly37@gmail.com>
2024-09-13 17:19:05 +0200

    added same event correlation in the toy model as well as same event correlation plot

--- toyModel.C
+++ toyModel.C
@@ -11,10 +11,10 @@
 using namespace std;
 
 void toyModel(int number_of_events = 100,
-              int number_of_particles = 100,       // number of particles PER event
-              int number_of_jet_particles = 100,   // same for recoil jets
-              bool show_recoil_jet = true,        // this is the jet emitted opposite in azimuth to the single jet
-              bool is_isotropic = false)            // is the background isotropic or do we have flow
+              int number_of_particles = 1000,  // number of particles PER event
+              int number_of_jet_particles = 100,  // same for recoil jets
+              bool show_recoil_jet = true,  // this is the jet emitted opposite in azimuth to the single jet
+              bool is_isotropic = false)  // is the background isotropic or do we have flow
 {
     // gStyle->SetOptStat(111111);
     gStyle->SetPalette(kDeepSea);
@@ -25,33 +25,39 @@ void toyModel(int number_of_events = 100,
     TCanvas *c1 = new TCanvas();
     c1->cd(); // I think this sets c1 as the canvas to draw on
 
-    // creating the 2D histogram
+    // creating the 2D histogram for the phi eta plot
     TH2F *hist = new TH2F("hist", "Isotropically Generated Event(s)",
                           100, 0, 2*TMath::Pi(), // x axis
                           100, -1, 1);           // y axis
     hist->SetStats(0); // to get rid of the legend
 
-    // vectors to store values of phi and eta
-    vector<double> phi_values = {};
-    vector<double> eta_values = {};
+    // creating the 2D histogram for the delta_phi delta_eta plot
+    TH2F *delta_hist = new TH2F("delta_hist", "Two Particle Correlation",
+                          100, 0, 2*TMath::Pi(), // x axis
+                          100, -2, 2);           // y axis
+    delta_hist->SetStats(0); // to get rid of the legend
 
     // Flow function
     TF1* flow = new TF1("flow", "1 + 2*[0]*cos(2*(x - [1]))", 0, 2*TMath::Pi()); // [0],[1] are the parameters of the function
 
+    // initialize a random number generator
+    gRandom = new TRandom3(0);
+
+    // phi is in an interval [0, 2pi], these are the endpoints for the interval
+    double phi_min = 0;
+    double phi_max = 2*TMath::Pi();
+
+    // eta in [-1, 1]
+    double eta_min = -1;
+    double eta_max = 1;
+
     // isotropic multi-event generator
     for(int i = 0; i < number_of_events; i++)
     {
-        // initialize a random number generator
-        gRandom = new TRandom3(0);
-
-        // phi is in an interval [0, 2pi], these are the endpoints for the interval
-        double phi_min = 0;
-        double phi_max = 2*TMath::Pi();
-
-        // eta in [-1, 1]
-        double eta_min = -1;
-        double eta_max = 1;
-
+        // vectors to store values of phi and eta
+        vector<double> phi_values = {};
+        vector<double> eta_values = {};
+        
         // generating random values for phi from the above interval (background)
         if (is_isotropic)
         {
@@ -134,51 +140,44 @@ void toyModel(int number_of_events = 100,
             }
         }
 
+        // SAME EVENT TWO PARTICLE CORRELATION
+        for(int i = 0; i < phi_values.size(); ++i)
+        {
+            for(int j = 0; j < phi_values.size(); ++j)
+            {
+                if (j == i) {continue;}
+
+                double delta_phi = phi_values[i] - phi_values[j];
+                double delta_eta = eta_values[i] - eta_values[j];
+
+                // accounting for the periodic nature of delta_phi
+                while (delta_phi > 2*TMath::Pi())
+                {
+                    delta_phi -= 2*TMath::Pi();
+                }
+
+                while (delta_phi < 0)
+                {
+                    delta_phi += 2*TMath::Pi();
+                }
+                
+                delta_hist->Fill(delta_phi, delta_eta);
+            }
+        }
+
     }
 
     hist->GetXaxis()->SetTitle("#phi");
     hist->GetYaxis()->SetTitle("#eta");
     hist->Draw("Colz"); // use Lego2 for fancy 3D plot and Colz for regular 2D plot
 
-    // TWO PARTICLE CORRELATION
-
     // creating a canvas to draw stuff on
     TCanvas *c2 = new TCanvas();
     c2->cd(); // selecting c2 to draw on
 
-    // creating the 2D histogram
-    TH2F *delta_hist = new TH2F("delta_hist", "Two Particle Correlation",
-                          100, 0, 2*TMath::Pi(), // x axis
-                          100, -2, 2);           // y axis
-    delta_hist->SetStats(0); // to get rid of the legend
-
-    for(int i = 0; i < phi_values.size(); ++i)
-    {
-        for(int j = 0; j < phi_values.size(); ++j)
-        {
-            if (j == i) {continue;}
-
-            double delta_phi = phi_values[i] - phi_values[j];
-            double delta_eta = eta_values[i] - eta_values[j];
-
-            // accounting for the periodic nature of delta_phi
-            while (delta_phi > 2*TMath::Pi())
-            {
-                delta_phi -= 2*TMath::Pi();
-            }
-
-            while (delta_phi < 0)
-            {
-                delta_phi += 2*TMath::Pi();
-            }
-            
-            delta_hist->Fill(delta_phi, delta_eta);
-        }
-    }
-
     delta_hist->GetXaxis()->SetTitle("#Delta#phi");
     delta_hist->GetYaxis()->SetTitle("#Delta#eta");
-    delta_hist->Draw("Colz"); // use Lego2 for fancy 3D plot and Colz for regular 2D plot
+    delta_hist->Draw("Lego2"); // use Lego2 for fancy 3D plot and Colz for regular 2D plot
 
     
 }
