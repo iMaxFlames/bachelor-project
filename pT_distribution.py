5601f9fed662c92fbefcea047b5d1fdf76b7e54e
Ayush Chakraborty <chak.polly37@gmail.com>
2024-11-05 14:35:07 +0100

    changed the trigger to  final state hadron, from hard process

--- pT_distribution.py
+++ pT_distribution.py
@@ -10,79 +10,88 @@ working_path = path.join(home, "jetscape-docker", "X-SCAPE", "build")
 data_filename = "PythiaGun_MATTER_MARTINI_Hydro.dat"
 
 # Define the headers
-start_header = "# HardProcess Parton List: PythiaGun"
-end_header = "# Energy loss Shower Initating Parton: JetEnergyLoss"
+hard_process_header = "# HardProcess Parton List: PythiaGun"
+
+hadron_header_start = "# Final State Hadrons"
+hadron_header_stop = "Event"
 
 # Initialize a list to store the lines between the headers
-event_list = []
-event_i = []
+hard_process_list = []
+
+event_list = [] # List  of all events
+event_i = [] # List of all final state hadrons in event i
 
 # Open the file and read it line by line
 with open(path.join(working_path, data_filename), 'r') as file:
-    capture = False
+    hard_process_capture = False
+    hadron_capture = False
     for line in file:
-        # Check if we've reached the start header
-        if start_header in line:
-            capture = True
+        # Extracting the hard scattering process for each event (just the first line)
+        if hard_process_header in line:
+            hard_process_capture = True
             continue  # Skip the start header line
         
-        # Check if we've reached the end header
-        if end_header in line:
-            capture = False
+        if hard_process_capture:
+            hard_process_list.append(line.strip())
+            hard_process_capture = False
+            continue
+
+
+        # Extracting all the final state hadrons from all events
+        if hadron_header_start in line:
+            hadron_capture = True
+            continue
+
+        if hadron_header_stop in line:
+            hadron_capture = False
             if len(event_i) > 0:
                 event_list.append(event_i) 
             event_i = []
             continue 
 
-        # If we're between headers, capture the line
-        if capture:
+        if hadron_capture:
             event_i.append(line.strip())
 
-# Display the captured lines
-# print("\n".join(lines_between_headers))
-
-# print(event_list[0][0])
-# line_split = event_list[0][0].split()
-# print(line_split)
-
-# print(line_split[-4], line_split[-3])
-# print(type(line_split[-4]))
-
+# Because there is not stop header for the hadrons at the end 
+event_list.append(event_i)
 
 def plot_all_events():
     plt.figure()
 
-    for event in event_list:
-        particle = event[0]
-        x = float(particle.split()[-4])
-        y = float(particle.split()[-3])
+    for event in hard_process_list:
+        x = float(event.split()[-4])
+        y = float(event.split()[-3])
         plt.scatter(x, y)
 
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.savefig("xy_for_all_events.pdf")
+    plt.savefig("Figures/xy_for_all_events.pdf")
     plt.show()
 
 
-def plot_high_pT_events():
+def plot_high_pT_events(high_pT_threshold=30.0):
     plt.figure()
 
-    for event in event_list:
-        for particle in event:
-            if float(particle.split()[3]) > 80.0:
-                x = float(particle.split()[-4])
-                y = float(particle.split()[-3])
+    for i in range(len(event_list)):
+        for hadron in event_list[i]:
+            pT = float(hadron.split()[5])
+            if pT >= high_pT_threshold:
+                x = float(hard_process_list[i].split()[-4])
+                y = float(hard_process_list[i].split()[-3])
                 plt.scatter(x, y)
                 break
+
+
     
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.savefig("xy_high_pT_events.pdf")
+    plt.savefig("Figures/xy_high_pT_events.pdf")
     plt.show()
 
 plot_high_pT_events()
 # plot_all_events()
+
